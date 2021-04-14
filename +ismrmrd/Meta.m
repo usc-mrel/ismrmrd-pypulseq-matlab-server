4555a5e481c6144f2e2ee406239533eee091f1e6
Kelvin Chow <kelvin.chow@gmail.com>
2021-04-14 02:35:54 -0500

    Partial refactor of ismrmrd.Meta class to support arbitrary fields.  Only the serialize() function is currently implemented.

--- +ismrmrd/Meta.m
+++ +ismrmrd/Meta.m
@@ -1,228 +1,76 @@
-classdef Meta < handle
-
-    properties
-        DataRole
-        ImageComment
-        SeriesDescription
-        ImageType
-        RescaleIntercept
-        RescaleSlope
-        WindowCenter
-        WindowWidth
-        LUTFileName
-        EchoTime
-        InversionTime
-        ROI
-        DirectSend
-    end
-
-    methods
-
-        function xmlstring = serialize(obj)
-
-        docNode = javaObject('org.apache.xerces.dom.DocumentImpl');
-        docNode.appendChild (docNode.createElement('ismrmrdMeta'));
-        docRootNode = docNode.getDocumentElement;
-
-            if ~isempty(obj.DataRole)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('DataRole'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(obj.DataRole));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.ImageComment)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('ImageComment'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(obj.ImageComment));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.SeriesDescription)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('SeriesDescription'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(obj.SeriesDescription));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.ImageType)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('ImageType'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(obj.ImageType));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.RescaleIntercept)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('RescaleIntercept'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(double2str(obj.RescaleIntercept)));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-            if ~isempty(obj.RescaleSlope)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('RescaleSlope'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(double2str(obj.RescaleSlope)));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-            if ~isempty(obj.WindowCenter)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('WindowCenter'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(int2str(obj.WindowCenter)));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.WindowWidth)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('WindowWidth'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(int2str(obj.WindowWidth)));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.LUTFileName)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('LUTFileName'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(obj.LUTFileName));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.EchoTime)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('EchoTime'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(double2str(obj.EchoTime)));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.InversionTime)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('InversionTime'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(double2str(obj.InversionTime)));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.ROI)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('ROI'));
-
-                valueNode = docNode.createElement('value');
-                valueNode.appendChild(docNode.createTextNode(obj.ROI));
-
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
-            end
-
-            if ~isempty(obj.DirectSend)
-                metaNode = docNode.createElement('meta');
-
-                nameNode = docNode.createElement('name');
-                nameNode.appendChild(docNode.createTextNode('DirectSend'));
-
-                valueNode = docNode.createElement('value');
-                if obj.DirectSend
-                    valueNode.appendChild(docNode.createTextNode('true'));
-                else
-                    valueNode.appendChild(docNode.createTextNode('false'));
+classdef Meta
+    methods (Static)
+        function xml = serialize(s)
+            % Take a struct and turn it in to an XML-formatted MetaAttribute
+            % Each struct field comprises a "meta" attribute with a "name" equal to 
+            % its field name and a value equal to its field value.  Numeric or cell
+            % arrays are supported.
+            docNode = javaObject('org.apache.xerces.dom.DocumentImpl');
+            docNode.appendChild (docNode.createElement('ismrmrdMeta'));
+            docRootNode = docNode.getDocumentElement;
+
+            fields = fieldnames(s);
+
+            for iField = 1:numel(fields)
+                if ~isempty(fields{iField})
+                    % Flag to discard this MetaAttribute if it contains invalid values
+                    isGood = true;
+
+                    val = s.(fields{iField});
+
+                    if isempty(val)
+                        continue
+                    end
+
+                    metaNode = docNode.createElement('meta');
+                    nameNode = docNode.createElement('name');
+                    nameNode.appendChild(docNode.createTextNode(fields{iField}));
+                    metaNode.appendChild(nameNode);
+
+                    % Turn scalars and numerical arrays into cell arrays
+                    if ~iscell(val)
+                        if ~isscalar(val) && ~ischar(val)
+                            val = num2cell(val);
+                        else
+                            val = {val};
+                        end
+                    end
+
+                    for iVal = 1:numel(val)
+                        valueNode = docNode.createElement('value');
+                        % chars are stored directly
+                        if ischar(val{iVal})
+                            valueNode.appendChild(docNode.createTextNode(val{iVal}));
+
+                        % logicals are stored as 'true' or 'false'
+                        elseif islogical(val{iVal})
+                            if val{iVal}
+                                valueNode.appendChild(docNode.createTextNode('true'));
+                            else
+                                valueNode.appendChild(docNode.createTextNode('false'));
+                            end
+
+                        % integers are stored without decimals
+                        elseif isinteger(val{iVal})
+                            valueNode.appendChild(docNode.createTextNode(int2str(val{iVal})));
+
+                        % all other numerics (double/singles) are stored with 16 decimals
+                        elseif isnumeric(val{iVal})
+                            valueNode.appendChild(docNode.createTextNode(num2str(val{iVal}, ' %.16f')));
+
+                        else
+                            warning('Unsupported class of type %s for Meta field %s', class(val{iVal}), fields{iField})
+                            isGood = false;
+                            continue
+                        end
+                        metaNode.appendChild(valueNode);
+                    end
+                    if isGood
+                        docRootNode.appendChild(metaNode);
+                    end
                 end
-                metaNode.appendChild(nameNode);
-                metaNode.appendChild(valueNode);
-
-                docRootNode.appendChild(metaNode);
             end
-
-            xmlstring = char(xmlwrite(docNode));
-            % xmlstring(end+1) = char(0);
+            xml = char(xmlwrite(docNode));
         end
-
-    end
+    end % methods (Static)
 end
